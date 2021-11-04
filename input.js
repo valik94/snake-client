daa0179bb4c120b4af025c59841a51af3eaa1bee
valik94 <valentin.lyashenko@ryerson.ca>
2021-11-04 04:09:52 +0000

    input module added to snake excercise (play.js) to handle user input for the game.

---
+++ input.js
@@ -0,0 +1,25 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  console.log("received " + data);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connect.write("Move: up")
+  }
+  if (key === 'a') {
+    connect.write("Move: left")
+  }
+};
+
+module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,30 +1,8 @@
 const net = require("net");
-const {connect} = require('./client')
-// establishes a connection with the game server
+const {connect} = require('./client');
+const {setupInput} = require ('./input');
 
+// establishes a connection with the game server
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  console.log("received " + data);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  if (key === 'w') {
-    connect.write("Move: up")
-  }
-  if (key === 'a') {
-    connect.write("Move: left")
-  }
-};
